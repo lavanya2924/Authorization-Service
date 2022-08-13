c72fdce7d573292b846349e3cb707e0907ca045e
lavanya2924 <lavs291198@gmail.com>
2022-08-13 14:19:13 +0530

    nothing

--- src/main/java/com/pension/AuthorizationApplication.java
+++ src/main/java/com/pension/AuthorizationApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@Configuration
 //@EnableSwagger2
 public class AuthorizationApplication {  
-	
-	
+		
 	public static void main(String[] args) {
 		SpringApplication.run(AuthorizationApplication.class, args);
 	}
